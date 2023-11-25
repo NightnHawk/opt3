0d6ce8f826a32ca75d66b379a6056ed07034ad08
NightnHawk <grzegorz_lazinski@o2.pl>
2023-11-25 03:26:16 +0100

    Nelder-Mead Minimization

--- methods.py
+++ methods.py
@@ -2,7 +2,11 @@
 #     i = 0;
 #     while abs(x - x1) < epsilon:
 #         i += 1
+import sys
+import warnings
+
 import numpy as np
+
 
 class functionWrapper:
     def __init__(self, f):
@@ -22,45 +26,121 @@ class functionWrapper:
     def reset_count(self):
         self.counter = 0
 
-def nelder_mead(f, x, s_init, alpha:float, beta:float, gamma:float, delta:float, epsilon:float, nMax:int):
-    fun = functionWrapper(f)
-    e = np.eye(len(x))
-    p = np.array(len(x) + 1)
-    simplexSet = np.empty(len(x) + 1)
-    pMin = None
-    pMax = None
-    p[0] = x
-    for i in range(1, len(x)):
-        p[i] = p[0] + s_init * e[i]
-    for i in range(0, len(x) + 1):
-        simplexSet[i] = fun.function(p[i])
-    simplexSorted = simplexSet.copy().sort()
-    minimum = simplexSorted[0]
-    maximum = simplexSorted[-1]
-    indexMin = np.where(simplexSet == minimum)[0]
-    pMin = p[indexMin]
-    indexMax = np.where(simplexSet == maximum)[0]
-    pMax = p[indexMax]
-    p_ = (sum(simplexSet) - pMax) / len(x)
-    pOdb = p_ + alpha * (p_ - pMax)
-    if fun.function(pOdb) < fun.function(pMin):
-        pe = p_ + gamma * (pOdb - p_)   #pe p z ekspansji
-        if fun.function(pe) < fun.function(pOdb):
-            pMax = pe
-        else:
-            pMax = pOdb
-    else:
-        if fun.function(pMin) <= fun.function(pOdb) < f(pMax):
-            pMax = pOdb
-        else:
-            pz = p_ + beta * (pMax - p_)
-            if fun.function(pz) >= fun.function(pMax):
-                for i in range(len(x)):
-                    if i != indexMin:
-                        p[i] = delta * (p[i] + pMin)
-            else:
-                pMax = pz
-    if fun.get_count() > nMax:
-        raise ValueError
 
-    return pMin
+def objective_function(x):
+    return np.sin(np.pi * np.sqrt((x[0]/np.pi)**2 + (x[1]/np.pi)**2)/(np.pi * np.sqrt(((x[0]/np.pi)**2 + (x[1]/np.pi)**2))))
+
+def nelder_mead(func, x0, s0, alpha: float, beta: float, gamma: float, delta: float, epsilon: float, nMax: int, bounds=None):
+    f = functionWrapper(func)
+    N = len(x0)
+    e = np.eye(len(x0))
+    x0 = np.atleast_1d(x0).flatten()  # make sure that x0 is a flat array of at least one dimension
+    x0 = np.asfarray(x0, dtype=np.double)  # make sure that all x0 values are stored as float64 type values
+    simplex = np.empty((N + 1, N), dtype=np.double)  # prepare an array for simplex vertices to be stored
+    if bounds is not None:
+        lower_bound, upper_bound = bounds
+        simplex[0] = np.clip(x0, lower_bound, upper_bound)
+    else:
+        simplex[0] = x0  # set first vertex as x0
+    if bounds is not None:
+        x0 = np.clip(x0, lower_bound, upper_bound)
+        for i in range(1, N):
+            point = simplex[0] + s0 * e[i]
+            simplex[i] = np.clip(point, lower_bound, upper_bound)
+            # set rest of vertices but calculating the value moved my the length of s_init in the direction of basis vector e
+    else:
+        for i in range(1, N):
+            simplex[i] = simplex[0] + s0 * e[i]
+
+    index_min = np.argmin(np.array([f.f(point) for point in simplex]))
+    simplex_min = simplex[index_min]
+    index_max = np.argmax(np.array([f.f(point) for point in simplex]))
+    simplex_max = simplex[index_max]
+
+    simplex_centroid = np.sum(np.delete(simplex, index_max)) / N
+    simplex_reflection = simplex_centroid + alpha * (simplex_centroid - simplex_max)
+    if f.f(simplex_reflection) < f.f(simplex_min):
+        simplex_expansion = simplex_centroid + gamma * (simplex_reflection - simplex_centroid)
+
+        if bounds is not None:
+            simplex_expansion = np.clip(simplex_expansion, lower_bound, upper_bound)
+
+        if f.f(simplex_expansion) < f.f(simplex_reflection):
+            simplex_max = simplex_expansion
+        else:
+            simplex_max = simplex_reflection
+    else:
+        if f.f(simplex_min) <= f.f(simplex_reflection) < f.f(simplex_max):
+            simplex_max = simplex_reflection
+        else:
+            simplex_contraction = simplex_centroid + beta * (simplex_max - simplex_centroid)
+            if f.f(simplex_contraction) >= f.f(simplex_max):
+                for i in range(N):
+                    if i != index_min:
+                        simplex[i] = delta * (simplex[i] + simplex_min)
+            else:
+                simplex_max = simplex_contraction
+
+    if bounds is not None:
+        simplex_max = np.clip(simplex_max, lower_bound, upper_bound)
+
+    if f.get_count() > nMax:
+        raise ValueError('Function call count exceeded')
+
+    difference_max = sys.float_info.max
+    while difference_max < epsilon:
+        index_min = np.argmin(np.array([f.f(point) for point in simplex]))
+        simplex_min = simplex[index_min]
+        index_max = np.argmax(np.array([f.f(point) for point in simplex]))
+        simplex_max = simplex[index_max]
+
+        simplex_centroid = np.sum(np.delete(simplex, index_max)) / N
+        simplex_reflection = simplex_centroid + alpha * (simplex_centroid - simplex_max)
+
+        if bounds is not None:
+            simplex_reflection = np.clip(simplex_reflection, lower_bound, upper_bound)
+
+        if f.f(simplex_reflection) < f.f(simplex_min):
+            simplex_expansion = simplex_centroid + gamma * (simplex_reflection - simplex_centroid)
+
+            if bounds is not None:
+                simplex_expansion = np.clip(simplex_expansion, lower_bound, upper_bound)
+
+            if f.f(simplex_expansion) < f.f(simplex_reflection):
+                simplex_max = simplex_expansion
+            else:
+                simplex_max = simplex_reflection
+
+            if bounds is not None:
+                simplex_max = np.clip(simplex_max, lower_bound, upper_bound)
+
+        else:
+            if f.f(simplex_min) <= f.f(simplex_reflection) < f.f(simplex_max):
+                simplex_max = simplex_reflection
+
+                if bounds is not None:
+                    simplex_max = np.clip(simplex_max, lower_bound, upper_bound)
+
+            else:
+                simplex_contraction = simplex_centroid + beta * (simplex_max - simplex_centroid)
+
+                if bounds is not None:
+                    simplex_contraction = np.clip(simplex_contraction, lower_bound, upper_bound)
+
+                if f.f(simplex_contraction) >= f.f(simplex_max):
+                    for i in range(N):
+                        if i != index_min:
+                            simplex[i] = delta * (simplex[i] + simplex_min)
+                    simplex = np.clip(simplex, lower_bound, upper_bound)
+                else:
+                    simplex_max = simplex_contraction
+
+                    if bounds is not None:
+                        simplex_max = np.clip(simplex_max, lower_bound, upper_bound)
+
+        if f.get_count() > nMax:
+            raise ValueError('Function call count exceeded')
+
+        difference = [simplex_min - simplex[i] for i in range(len(simplex) - 1)]
+        difference_max = max(difference)
+    return simplex_min, f.get_count()
---
+++ test.py
@@ -0,0 +1,32 @@
+import random
+import numpy as np
+from scipy.optimize import minimize
+
+from methods import nelder_mead, objective_function
+
+a_array = [4., 4.4934, 5.]
+
+
+def g1(x1):
+    return -x1 + 1  # x1 >= 1
+
+
+def g2(x2):
+    return -x2 + 1  # x2 >= 1
+
+
+def g3(x1, x2, a):
+    return np.sqrt(x1 ** 2 + x2 ** 2) - a   #g3(x1,x2) <=0
+
+
+bounds = [(1., - np.sqrt(15)), (4., np.sqrt(15))]
+randSet = []
+for i in range(10):
+    randSet.append([random.uniform(1., 4.), random.uniform(- np.sqrt(15), np.sqrt(15))])
+
+for i in range(10):
+    x_min = nelder_mead(objective_function, x0=randSet[i], s0=0.5, alpha=1., beta=0.5, gamma=2., delta=0.5, epsilon=0.5,
+                        nMax=1000, bounds=bounds)
+    print(x_min, ', ', objective_function(x_min))
+    x_min_build_in = minimize(fun=objective_function, x0=randSet[i], method='Nelder-Mead')
+    print(x_min_build_in.x)
